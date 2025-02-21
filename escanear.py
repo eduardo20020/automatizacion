22f4cdb794e4d0c480fb50abb0456ab98370b0a6
Brandon Sanchez <brandonde2002@gmail.com>
2025-02-21 11:18:31 -0600

    primer commit

--- escanear.py
+++ escanear.py
@@ -9,8 +9,9 @@ def search_files(directorio):
 
       if os.path.isdir(ruta_completa):
         search_files(ruta_completa)
-        print(f"\t\t\tCARPETA: {item}  VACIA")
+        print(f"\t\tCARPETA VACIA: {item}")
       else:
+        print(f"\t\t\t\tCARPETA: {item}")
         size_gb = os.stat(ruta_completa).st_size / 1_073_741_824  # Convertir a GB
         print(ruta_completa, "-----", round(size_gb, 2), "GB")
 
